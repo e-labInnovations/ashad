f9e5f443fc1d309e44cb8bc5d3e19ec8eaf013df
e-labinnovations <mohammedashad23@gmail.com>
2022-08-10 21:37:24 +0530

    Custom HTML email for password reset

--- inc/mail-templates.php
+++ inc/mail-templates.php
@@ -1,7 +1,7 @@
 <?php
 
+//New user welcome email
 function ashad_wp_new_user_notification_email( $wp_new_user_notification_email, $user, $blogname ) {
-    global $wpcargo;
     $user_url = stripslashes( $user->user_url );
     $user_login = stripslashes( $user->user_login );
     $user_email = stripslashes( $user->user_email );
@@ -15,4 +15,43 @@ function ashad_wp_new_user_notification_email( $wp_new_user_notification_email, 
     $wp_new_user_notification_email['headers'] = array('Content-Type: text/html; charset=UTF-8');
     return $wp_new_user_notification_email;
 }
-add_filter( 'wp_new_user_notification_email', 'ashad_wp_new_user_notification_email', 10, 3 );
+add_filter('wp_new_user_notification_email', 'ashad_wp_new_user_notification_email', 10, 3);
+
+//Password reset email (Setting email as html email)
+function ashad_retrieve_password_notification_email($defaults) {
+    $defaults['headers'] = array('Content-Type: text/html; charset=UTF-8');
+    return $defaults;
+}
+add_filter('retrieve_password_notification_email', 'ashad_retrieve_password_notification_email', 10, 3);
+
+//Password reset email (Setting html message content)
+function ashad_reset_password_message($message, $key, $user_login, $user_data )    {
+    $user_fullname = $user_data->user_firstname . ' ' . $user_data->user_last_name;
+    $blog_name = get_bloginfo('name');
+    $reset_url = network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login');
+    ob_start();
+    include(get_stylesheet_directory() .  '/templates/mail/password-change.php');
+    $message = ob_get_clean();
+    return $message;
+}
+add_filter("retrieve_password_message", "ashad_reset_password_message", 99, 4);
+
+
+//Template testing purpose
+/*
+add_action( 'init',  function() {
+    add_rewrite_rule( 'password-change', 'index.php?password-change=true', 'top' );
+} );
+
+add_filter( 'query_vars', function( $query_vars ) {
+    $query_vars[] = 'password-change';
+    return $query_vars;
+} );
+add_action( 'template_include', function( $template ) {
+    if ( get_query_var( 'password-change' ) == false) {
+        return $template;
+    }
+ 
+    return get_template_directory() . '/templates/mail/password-change.php';
+} );
+*/
---
+++ templates/mail/password-change.php
@@ -0,0 +1,437 @@
+<?php
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+
+<head>
+	<title>Reset Password</title>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+	<!--[if !mso]><!-->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" type="text/css" />
+	<!--<![endif]-->
+	<style>
+		* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		a[x-apple-data-detectors] {
+			color: inherit !important;
+			text-decoration: inherit !important;
+		}
+
+		#MessageViewBody a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		p {
+			line-height: inherit
+		}
+
+		.desktop_hide,
+		.desktop_hide table {
+			mso-hide: all;
+			display: none;
+			max-height: 0px;
+			overflow: hidden;
+		}
+
+		@media (max-width:620px) {
+			.desktop_hide table.icons-inner {
+				display: inline-block !important;
+			}
+
+			.icons-inner {
+				text-align: center;
+			}
+
+			.icons-inner td {
+				margin: 0 auto;
+			}
+
+			.image_block img.big,
+			.row-content {
+				width: 100% !important;
+			}
+
+			.mobile_hide {
+				display: none;
+			}
+
+			.stack .column {
+				width: 100%;
+				display: block;
+			}
+
+			.mobile_hide {
+				min-height: 0;
+				max-height: 0;
+				max-width: 0;
+				overflow: hidden;
+				font-size: 0px;
+			}
+
+			.desktop_hide,
+			.desktop_hide table {
+				display: table !important;
+				max-height: none !important;
+			}
+		}
+	</style>
+</head>
+
+<body style="background-color: #d9dffa; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+	<table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation"
+		style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #d9dffa;" width="100%">
+		<tbody>
+			<tr>
+				<td>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1"
+						role="presentation"
+						style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #cfd6f4;" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation"
+										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px;"
+										width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 20px; padding-bottom: 0px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+													width="100%">
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="image_block block-1" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+														width="100%">
+														<tr>
+															<td class="pad"
+																style="width:100%;padding-right:0px;padding-left:0px;">
+																<div align="center" class="alignment"
+																	style="line-height:10px"><img
+																		alt="Card Header with Border and Shadow Animated"
+																		class="big" src="<?php echo get_template_directory_uri() . '/assets/img/email/password-change/animated_header.gif' ?>"
+																		style="display: block; height: auto; border: 0; width: 600px; max-width: 100%;"
+																		title="Card Header with Border and Shadow Animated"
+																		width="600" /></div>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2"
+						role="presentation"
+						style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #d9dffa; background-image: url('<?php echo get_template_directory_uri() . '/assets/img/email/password-change/body_background_2.png' ?>'); background-position: top center; background-repeat: repeat;"
+						width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation"
+										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px;"
+										width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-left: 50px; padding-right: 50px; vertical-align: top; padding-top: 15px; padding-bottom: 15px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+													width="100%">
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-1" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #506bec; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p style="margin: 0; font-size: 14px;">
+																			<strong><span style="font-size:38px;">Forgot
+																					your password?</span></strong></p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-2" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #40507a; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p style="margin: 0; font-size: 14px;"><span
+																				style="font-size:16px;">Hi
+																				<?php echo $user_fullname; ?>, we received a request
+																				to reset your password.</span></p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-3" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #40507a; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p style="margin: 0; font-size: 14px;"><span
+																				style="font-size:16px;">Let’s get you a
+																				new one!</span></p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="button_block block-4" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+														width="100%">
+														<tr>
+															<td class="pad"
+																style="padding-bottom:20px;padding-left:10px;padding-right:10px;padding-top:20px;text-align:left;">
+																<div align="left" class="alignment">
+																	<!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="<?php echo $reset_url; ?>" style="height:46px;width:186px;v-text-anchor:middle;" arcsize="35%" stroke="false" fillcolor="#506bec"><w:anchorlock/><v:textbox inset="5px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:15px"><![endif]--><a
+																		href="<?php echo $reset_url; ?>"
+																		style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#506bec;border-radius:16px;width:auto;border-top:0px solid TRANSPARENT;font-weight:400;border-right:0px solid TRANSPARENT;border-bottom:0px solid TRANSPARENT;border-left:0px solid TRANSPARENT;padding-top:8px;padding-bottom:8px;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;"
+																		target="_blank"><span
+																			style="padding-left:25px;padding-right:20px;font-size:15px;display:inline-block;letter-spacing:normal;"><span
+																				dir="ltr"
+																				style="word-break: break-word;"><span
+																					data-mce-style="" dir="ltr"
+																					style="line-height: 30px;"><strong>RESET
+																						PASSWORD</strong></span></span></span></a>
+																	<!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-5" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #40507a; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p style="margin: 0; font-size: 14px;"><span
+																				style="font-size:14px;">Having trouble?
+																				<a href="mailto:<?php echo get_option( 'admin_email' ); ?>"
+																					rel="noopener"
+																					style="text-decoration: none; color: #40507a;"
+																					target="_blank"
+																					title="<?php echo get_option( 'admin_email' ); ?>"><strong><?php echo get_option( 'admin_email' ); ?></strong></a></span>
+																		</p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-6" role="presentation" width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #40507a; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p style="font-size:14px;">Didn't request a
+																			password reset? You can ignore this message.
+																		</p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-3"
+						role="presentation" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation" style="width:600px" width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="vertical-align:top;padding-top:0px;padding-bottom:5px;border-top:0px;border-right:0px;border-bottom:0px;border-left:0px;"
+													width="100%">
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="image_block block-1" role="presentation" width="100%">
+														<tr>
+															<td class="pad"
+																style="width:100%;padding-right:0px;padding-left:0px;">
+																<div align="center" class="alignment"
+																	style="line-height:10px"><img
+																		alt="Card Bottom with Border and Shadow Image"
+																		class="big" src="<?php echo get_template_directory_uri() . '/assets/img/email/password-change/bottom_img.png' ?>"
+																		style="width: 600px; max-width:100%"
+																		title="Card Bottom with Border and Shadow Image"
+																		width="600" /></div>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-4"
+						role="presentation" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation" style="width:600px" width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="padding-left:10px;padding-right:10px;vertical-align:top;padding-top:10px;padding-bottom:20px;border-top:0px;border-right:0px;border-bottom:0px;border-left:0px;"
+													width="100%">
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="image_block block-1" role="presentation" width="100%">
+														<tr>
+															<td class="pad">
+																<div align="center" class="alignment"
+																	style="line-height:10px"><a
+																		href="<?php echo get_option( 'siteurl' ); ?>"
+																		style="outline:none" tabindex="-1"
+																		target="_blank"><img alt="e-lab innovations Logo"
+																			src="https://elabins.com/wp-content/uploads/2022/02/elab-website-logo-png-01.png"
+																			style="width: 145px; max-width:100%"
+																			title="e-lab innovations Logo" width="145" /></a></div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="social_block block-2" role="presentation" width="100%">
+														<tr>
+															<td class="pad">
+																<div class="alignment" style="text-align:center;">
+																	<table border="0" cellpadding="0" cellspacing="0"
+																		class="social-table" role="presentation"
+																		style="display:inline-block;" width="72px">
+																		<tr>
+																			<td style="padding:0 2px 0 2px;"><a
+																					href="https://www.instagram.com/"
+																					target="_blank"><img alt="Instagram"
+																						height="32"
+																						src="<?php echo get_template_directory_uri() . '/assets/img/email/password-change/instagram2x.png' ?>"
+																						title="instagram"
+																						width="32" /></a></td>
+																			<td style="padding:0 2px 0 2px;"><a
+																					href="https://www.twitter.com/"
+																					target="_blank"><img alt="Twitter"
+																						height="32"
+																						src="<?php echo get_template_directory_uri() . '/assets/img/email/password-change/twitter2x.png' ?>"
+																						title="twitter"
+																						width="32" /></a></td>
+																		</tr>
+																	</table>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-3" role="presentation" width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #97a2da; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p style="font-size:14px;text-align:center;">
+																			Please feel free to contact us at
+																			<?php echo get_option( 'admin_email' ); ?>.</p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table border="0" cellpadding="10" cellspacing="0"
+														class="text_block block-4" role="presentation" width="100%">
+														<tr>
+															<td class="pad">
+																<div style="font-family: sans-serif">
+																	<div class="txtTinyMce-wrapper"
+																		style="font-size: 14px; mso-line-height-alt: 16.8px; color: #97a2da; line-height: 1.2; font-family: Helvetica Neue, Helvetica, Arial, sans-serif;">
+																		<p
+																			style="margin: 0; text-align: center; font-size: 12px;">
+																			<span style="font-size:12px;">Copyright©
+																			<?php echo date("Y"); ?> <?php echo $blogname; ?>.</span></p>
+																		<p id="m_8010100107078456808text01"
+																			style="margin: 0; text-align: center; font-size: 12px;">
+																			<span style="font-size:12px;"><a
+																					href="<?php echo get_option( 'siteurl' ); ?>"
+																					rel="noopener"
+																					style="text-decoration: underline; color: #97a2da;"
+																					target="_blank"
+																					title="Unsubscribe ">Unsubscribe</a>
+																				| <a href="<?php echo get_option( 'siteurl' ); ?>"
+																					rel="noopener"
+																					style="text-decoration: underline; color: #97a2da;"
+																					target="_blank"
+																					title="Manage your preferences">Manage
+																					your preferences</a> | <a
+																					href="<?php echo get_option( 'siteurl' ); ?>"
+																					rel="noopener"
+																					style="text-decoration: underline; color: #97a2da;"
+																					target="_blank"
+																					title="Privacy Policy">Privacy
+																					Policy</a></span></p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+
+
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+
+
+	<!-- End -->
+</body>
+
+</html>
